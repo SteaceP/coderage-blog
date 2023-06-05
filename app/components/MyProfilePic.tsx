b6977453d3c3401cc93d9a2e87efbf6d329316b4
SteaceP <steacy@live.ca>
2023-06-04 21:43:23 -0400

    added next-cloudinary

--- app/components/MyProfilePic.tsx
+++ app/components/MyProfilePic.tsx
@@ -1,13 +1,16 @@
-import Image from 'next/image';
+'use client';
+
+import { CldImage } from 'next-cloudinary';
 
 export default function MyProfilePic() {
   return (
     <section className='w-full mx-auto'>
-      <Image
+      <CldImage
         className='border-4 border-black dark:border-slate-500 drop-shadow-xl shadow-black rounded-full mx-auto mt-8'
-        src='/images/profile-photo-600x600.jpg'
         width={200}
         height={200}
+        src='v1685926881/profile-photo-600x600_jiq3ii.jpg'
+        sizes='100vw'
         alt='Steace'
         priority={true}
       />
