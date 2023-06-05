91229c663eb4b5bed38aab816fa242dffdc6d5fa
SteaceP <steacy@live.ca>
2023-06-04 20:51:32 -0400

    removed fa icons and deprecated section in eslint

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import { FaYoutube, FaTwitter, FaGithub, FaLaptop } from 'react-icons/fa';
+import { FaTwitter, FaGithub } from 'react-icons/fa';
 
 export default function Navbar() {
   return (
@@ -16,28 +16,14 @@ export default function Navbar() {
         <div className='flex flex-row justify-center sm:justify-evenly align-middle gap-4 text-white text-4xl lg:text-5xl'>
           <Link
             className='text-white/90 hover:text-white'
-            href='https://www.youtube.com/@DaveGrayTeachesCode'
-            rel='noreferrer noopener'
-          >
-            <FaYoutube />
-          </Link>
-          <Link
-            className='text-white/90 hover:text-white'
-            href='https://courses.davegray.codes/'
-            rel='noreferrer noopener'
-          >
-            <FaLaptop />
-          </Link>
-          <Link
-            className='text-white/90 hover:text-white'
-            href='https://github.com/gitdagray'
+            href='https://github.com/SteaceP'
             rel='noreferrer noopener'
           >
             <FaGithub />
           </Link>
           <Link
             className='text-white/90 hover:text-white'
-            href='https://twitter.com/yesdavidgray'
+            href='https://twitter.com/paquettesteacy'
             rel='noreferrer noopener'
           >
             <FaTwitter />
