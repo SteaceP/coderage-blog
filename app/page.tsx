1a2d96f2839318f9f26e48f195e47ee8d7f11433
SteaceP <steacy@live.ca>
2023-08-23 21:38:08 -0400

    removed useless posts

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,9 @@ import Posts from './components/Posts';
 export default function Home() {
   return (
     <main className='px-6 mx-auto'>
-      <p className='mt-12 pb-4 text-center text-4xl'>Hello world!</p>
+      <p className='mt-12 pb-4 text-center text-4xl'>
+        This Website is a work in progress!
+      </p>
       <Posts />
     </main>
   );
