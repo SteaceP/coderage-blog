a9ea725caaa360ffcea634057905a81659e4c388
SteaceP <steacy@live.ca>
2023-08-26 19:28:53 -0400

    First commit (kind of)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: "jit",
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
+  variants: {
+    extend: {
+      opacity: ["disabled"],
+    },
+  },
   plugins: [require("@tailwindcss/typography")],
 };
